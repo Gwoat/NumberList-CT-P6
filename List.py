f4e0da18e0c01048028a5e203feb1cdc9b6d9c78
Gwoat <77695380+Gwoat@users.noreply.github.com>
2021-03-12 12:13:48 -0800

    Update List.py

--- List.py
+++ List.py
@@ -4,18 +4,28 @@
 3. We need to provide choices to the user
     a. Add more values to a list
     b. Return a value at a specific index
-
 """
 
 def mainProgram():
-    myMilst = []
-    print("Hello, there! Lt's work with lists!")
-    print("Choose from the following options. Type a number below!")
-    choice = input("1. Ad to  a list , 2. Return the value at an index position!   ")
-    if choice == "1":
-        addToList()
-    elif choice == "2":
-        indexValues()
+    myList = []
+    try:
+        print("Hello, there! Lt's work with lists!")
+        print("Choose from the following options. Type a number below!")
+        choice = input("""1. Ad to  a list,
+2. Return the value at an index position!
+3. Random Search
+4.Quit Program""")
+
+
+
+        if choice == "1":
+            addToList()
+        elif choice == "2":
+            indexValues()
+        elif choice == "3":
+            randomSearch()
+        else:
+            break
 
     #TO ADD: 1. A way to loop the action, 2. A way to quit, 3. Think of repitition.
 
@@ -27,8 +37,15 @@ def addToList():
 
 
 def indexValues():
+    print("Curious about an index position? ME TOO!")
+    indexPos = input("What index position would you like to see?")
+    print(myList[random.randrandint(0, len(myList)-1)])
+
+    
 
 
 if __name__ == "__main__":
     mainProgram()
+
+
             
